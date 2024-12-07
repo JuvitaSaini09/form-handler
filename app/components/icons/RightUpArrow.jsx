9908f76cc9f905fe33d719dac9c20c17018162d6
Juvita Saini <juvitasaini@gmail.com>
2024-12-07 20:08:59 +0530

    Added Header in create form

---
+++ app/components/icons/RightUpArrow.jsx
@@ -0,0 +1,14 @@
+export default function RightUpArrow() {
+  return (
+    <svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path 
+        d="M1.563 7.63176L8.43765 0.757107M8.43765 0.757107V7.35677M8.43765 0.757107H1.83799" 
+        stroke="#959DA5" 
+        strokeWidth="1.5" 
+        strokeLinecap="round" 
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
--- app/create/page.js
+++ app/create/page.js
@@ -1,7 +1,25 @@
+import RightUpArrow from '../components/icons/RightUpArrow';
 export default function CreateForm() {
   return (
-    <main>
-        Create a Form
+    <main className="w-[640px] mx-auto min-h-screen" >
+       
+       { /* Header */}
+       <section className="h-14 border-[1px] border-t-0 border-gray-200 flex justify-between items-center px-6">
+       <input type="text" placeholder="Untitled form" className="outline-none border-0 font-semibold text-base" />
+       <button className="h-8 border-[1px] border-gray-200 rounded-xl pr-[14px] pl-4 flex items-center gap-2 hover:bg-gray-50">
+        <span className="text-gray-400 font-semibold text-sm">Preview</span>
+        <RightUpArrow />
+       </button>
+       </section>
+       {/* Main */}
+       {/* <section className="border-2">
+        
+        </section> */}
+       {/* Footer */}
+       {/* <section className="border-2>
+        
+        </section> */}
+
     </main>
   );
 }
