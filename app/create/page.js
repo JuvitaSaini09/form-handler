63664279eb587f0eb4fe1f6ec14453c97450dfd5
Juvita Saini <juvitasaini@gmail.com>
2024-12-08 02:06:59 +0530

    added icons for input types

--- app/create/page.js
+++ app/create/page.js
@@ -1,7 +1,9 @@
+import AddSymbol from "../components/icons/AddSymbol";
 import Draft from "../components/icons/Draft";
 import RightUpArrow from "../components/icons/RightUpArrow";
 import Tick from "../components/icons/Tick";
 export default function CreateForm() {
+    
   return (
     <main className="w-[640px] mx-auto h-screen flex flex-col overflow-hidden">
       {/* Header - fixed at top */}
@@ -18,8 +20,22 @@ export default function CreateForm() {
       </section>
 
       {/* Main - scrollable section */}
-      <section className="flex-1 overflow-y-auto border-x-[1px] border-gray-200 scrollbar-hide">
-        <div className="min-h-[1150px]"></div>
+      <section className="flex-1 overflow-y-auto border-x-[1px] border-gray-200 scrollbar-hide px-6 pb-20">
+        <div className="min-h-[1150px] border-2 border-red-500 pt-6 flex flex-col gap-6">
+          {/* Questions list */}
+          <div className="border-2 border-blue-500">
+            {" "}
+            here the list of input fields will be rendered when user will add
+            input fields
+          </div>
+          {/* Add question button */}
+          <div className="flex justify-center border-2 border-blue-500">
+          <button className="h-8 border-[1px] border-gray-200 rounded-xl pr-[14px] pl-4 flex items-center gap-2 hover:bg-gray-50">
+            <AddSymbol />
+            <span>Add Question</span>
+          </button>
+          </div>
+        </div>
       </section>
 
       {/* Footer - fixed at bottom */}
