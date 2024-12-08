ec637bee9c78640b7ad66f259254c01addfacd0f
Juvita Saini <juvitasaini@gmail.com>
2024-12-08 19:08:08 +0530

    added preview route

--- app/create/components/Header.js
+++ app/create/components/Header.js
@@ -1,7 +1,12 @@
 import RightUpArrow from "@/app/components/icons/RightUpArrow";
 import React from "react";
+import { useRouter } from 'next/navigation';
 
 const Header = ({ title, onTitleChange, isValid }) => {
+  const router = useRouter();
+  const handlePreviewClick=()=>{
+    router.push('/preview');
+  }
   return (
     <section className="sticky top-0 w-full h-14 border-[1px] border-t-0 border-gray-200 flex justify-between items-center px-6 bg-white">
       <input
@@ -12,6 +17,7 @@ const Header = ({ title, onTitleChange, isValid }) => {
         className="outline-none border-0 font-semibold text-base w-full max-w-[300px] overflow-x-auto whitespace-nowrap"
       />
       <button 
+      onClick={handlePreviewClick}
         disabled={!isValid}
         className={`h-8 border-[1px] border-gray-200 rounded-xl pr-[14px] pl-4 flex items-center gap-2 
           ${isValid ? 'hover:bg-gray-50 text-[#0D0D0D]' : 'text-gray-400 cursor-not-allowed'}`}
---
+++ app/preview/page.js
@@ -0,0 +1,9 @@
+
+export default function PreviewForm() {
+  return (
+    <main className="w-[640px] mx-auto h-screen flex flex-col overflow-hidden">
+      <h1>Preview Form</h1>
+      {/* We'll add the preview content later */}
+    </main>
+  );
+} 
