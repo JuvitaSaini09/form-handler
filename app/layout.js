f8d202a247150c874427e2f747ef2d83b9586b7c
Juvita Saini <juvitasaini@gmail.com>
2024-12-08 19:20:35 +0530

    added inter font theme

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,10 @@
 import "./globals.css";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: "Form Builder",
@@ -7,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         {children}
       </body>
